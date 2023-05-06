a10a81f964e6d86b40b2fb912db49992f6ec84b0
Solpio <ssusolpio@gmail.com>
2023-05-06 12:02:41 +0400

    https://argentinadev.atlassian.net/browse/RA-59

--- src/shared/components/breadcrumbs/style.ts
+++ src/shared/components/breadcrumbs/style.ts
@@ -5,12 +5,30 @@ export const BreadcrumbsWrapper = styled("div")`
 	display: flex;
 	gap: 10px;
 	font-size: 1.4rem;
+	@media (max-width: 1024px) {
+		flex-wrap: wrap;
+	}
+	@media (max-width: 450px) {
+		font-size: 1.2rem;
+	}
 `;
 
 export const Breadcrumb = styled("div")`
 	cursor: pointer;
 	position: relative;
 	color: ${(props) => color(props.theme.palette.mode).regularTabText};
+	overflow: hidden;
+	text-overflow: ellipsis;
+	height: 1.9rem;
+	@media (max-width: 1024px) {
+		max-width: 250px;
+	}
+	@media (max-width: 450px) {
+		max-width: 75px;
+	}
+	@media (max-width: 359px) {
+		max-width: 75px;
+	}
 	&:hover {
 		&:after {
 			position: absolute;
@@ -46,4 +64,7 @@ export const Separator = styled("div")`
 export const BreadcrumbWrapper = styled("div")`
 	display: flex;
 	gap: 10px;
+	@media (max-width: 640px) {
+		gap: 5px;
+	}
 `;
