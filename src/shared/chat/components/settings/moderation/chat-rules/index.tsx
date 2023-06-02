fe0306c2257353eb588df7229dfb43970843c97d
Petr Molchanov <p.molchanov@profsoft.pro>
2023-06-02 14:07:02 -0300

    https://argentinadev.atlassian.net/browse/RA-115

--- src/shared/chat/components/settings/moderation/chat-rules/index.tsx
+++ src/shared/chat/components/settings/moderation/chat-rules/index.tsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { TableWidget } from "shared/components/table-widget";
 import Switch from "shared/components/switch-widget";
 import Box from "@mui/material/Box";
 
 const ChatRules = () => {
+	const [value, setValue] = useState(false);
 	return (
 		<Box
 			sx={{
@@ -17,7 +18,11 @@ const ChatRules = () => {
 				title={"Admin List"}
 				content={[{ content: "ad", status: "ad" }]}
 			/>
-			<Switch description={"Allow chat admins to call bot commands"} />
+			<Switch
+				description={"Allow chat admins to call bot commands"}
+				value={value}
+				callback={(val) => setValue(val)}
+			/>
 		</Box>
 	);
 };
